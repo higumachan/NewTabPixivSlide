85e8eecbcc08dec655274e7fde4d60ce6217ba15
marin72_com <sifon1994@gmail.com>
2013-08-24 16:42:31 +0900

    今>>pixivの画像のURLを1つずつ配列に入れていく Next>>background.jsで取得したURLをHTMLで表示する部分を書く

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,16 @@
- // jQuery読み込み
-
+// pixivのAPI叩く
+$(function () {
+	var $images = $("#images");
+	var match;
+	$.get("http://spapi.pixiv.net/iphone/ranking.php?mode=day",
+		function (data) {
+			console.log("oooooo");
+			var comp = /http:\/\/(.+?)mw\.jpg/g;
+			match = data.match(comp);
+			$images.val(match[0]);
+		});
+	console.log(match);
+});
 
  
  // 右上のボタンが押された時
--- scripts/newtab.js
+++ scripts/newtab.js
@@ -1,24 +1,35 @@
-// pixivのAPI叩く
-$(function () {
+
+
+var str = $(function () {
 	$.get("http://spapi.pixiv.net/iphone/ranking.php?mode=day",
 		function (data) {
 			console.log("oooooo");
-			$.each(data, function() {
-				if (data.match(/http:\/\/(.+?)mw\.jpg/) !== -1){
-				//	$str.appendTo("#images");
-					console.log("ok");
-					return;
-				}
-			});
+			return (data);
 		});
 });
 
-$(function() {
-  // JSONデータをHTTP GETで取得
-  $.getJSON(
-    'data.js', // アクセス先のURL
-    function(data, status) {
-      console.log(data);
-    }
-  );
-});
+/*
+var siii = function;
+console.log(siii);
+
+var comp = /http:\/\/(.+?)mw\.jpg/;
+var res = comp.exec("http://i2.pixiv.net/img28/img/seeker00/mobile/37975653_128x128.jpg\",,,\"http://i2.pixiv.net/img28/img/seeker00/mobile/37975653_480mw.jpg\",,,\"2013-08-22 00:16:55\",\"東方 西行寺幽々子 風景 魂魄妖夢 東方Project100users入り\"\"http://i2.pixiv.net/img28/img/seeker00/mobile/37975653_480mw.jpg\",\"");
+console.log(res);
+
+var comp = /http:\/\/(.+?)mw\.jpg/;
+var res = comp.match("http://i2.pixiv.net/img28/img/seeker00/mobile/37975653_128x128.jpg\",,,\"http://i2.pixiv.net/img28/img/seeker00/mobile/37975653_480mw.jpg\",,,\"2013-08-22 00:16:55\",\"東方 西行寺幽々子 風景 魂魄妖夢 東方Project100users入り\"\"http://i2.pixiv.net/img28/img/seeker00/mobile/37975653_480mw.jpg\",\"");
+console.log(res);
+
+var source = "http://i2.pixiv.net/img28/img/seeker00/mobile/37975653_128x128.jpg\",,,\"http://i2.pixiv.net/img28/img/seeker00/mobile/37975653_480mw.jpg\",,,\"2013-08-22 00:16:55\",\"東方 西行寺幽々子 風景 魂魄妖夢 東方Project100users入り\"\"http://i2.pixiv.n975653_480mw.jpg\",\"";
+var comp = /http:\/\/(.+?)mw\.jpg/g;
+var arr = source.match(comp);
+console.log(arr);
+
+for (var i = 0; i < str.length; i++){
+	if (str[i] == '"'){
+		str
+	}
+}
+
+source.replace(/\"/, "")
+*/
