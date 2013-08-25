4b48cfd5fba1815b521b38f553da91fdcc4a289a
marin72_com <sifon1994@gmail.com>
2013-08-25 16:13:00 +0900

    Now is works!

--- scripts/background.js
+++ scripts/background.js
@@ -1,23 +1,34 @@
 // pixivのAPI叩く
-$(function () {
-	var $images = $("#images");
-	var match;
-	$.get("http://spapi.pixiv.net/iphone/ranking.php?mode=day",
-		function (data) {
-			console.log("oooooo");
-			var comp = /http:\/\/(.+?)mw\.jpg/g;
-			match = data.match(comp);
-			$images.val(match[0]);
-		});
-	console.log(match);
+var getURL = function(callback) {
+	$.get("http://spapi.pixiv.net/iphone/ranking.php?mode=day",　function(data) {
+		var match = data.match(/"http:\/\/[^"]+?mw\.jpg[^"]*?"/g);
+		// 画像を表示する
+		// document.getElementById("images").src = match[0];
+    var urls = $(match).map(function() {
+      return this.replace(/^"|"$/g, "");
+    });
+		callback(urls);
+    console.log(urls);
+  });
+};
+
+chrome.runtime.onInstalled.addListener(function () {
+  // newtab.jsに受け取る.
+  chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+    if (request.type == "getURL") {
+      getURL(function(urls) {
+        sendResponse({urls: urls});
+      });
+      return true;
+    }
+  });
+
+
+  // 右上のボタンが押された時
+  chrome.browserAction.onClicked.addListener(function () {
+    chrome.tabs.create({
+    　 url: chrome.extension.getURL("newtab/newtab.html")
+    });
+  });
 });
 
- 
- // 右上のボタンが押された時
-chrome.browserAction.onClicked.addListener(function () {
-	chrome.tabs.create({
-		url: chrome.extension.getURL("newtab/newtab.html")
-	});
-});
-
-// pixiv のAPIを叩きたい 
--- scripts/newtab.js
+++ scripts/newtab.js
@@ -1,35 +1,12 @@
+var showImages = function(urls){
+  $(urls).each(function() {
+    $("<img>").attr("src", this).appendTo("#images");
+  });
+};
 
-
-var str = $(function () {
-	$.get("http://spapi.pixiv.net/iphone/ranking.php?mode=day",
-		function (data) {
-			console.log("oooooo");
-			return (data);
-		});
+chrome.runtime.sendMessage({type: "getURL"}, function(response) {
+  console.log("new tab");
+  console.log(response.urls);
+  showImages(response.urls);
 });
 
-/*
-var siii = function;
-console.log(siii);
-
-var comp = /http:\/\/(.+?)mw\.jpg/;
-var res = comp.exec("http://i2.pixiv.net/img28/img/seeker00/mobile/37975653_128x128.jpg\",,,\"http://i2.pixiv.net/img28/img/seeker00/mobile/37975653_480mw.jpg\",,,\"2013-08-22 00:16:55\",\"東方 西行寺幽々子 風景 魂魄妖夢 東方Project100users入り\"\"http://i2.pixiv.net/img28/img/seeker00/mobile/37975653_480mw.jpg\",\"");
-console.log(res);
-
-var comp = /http:\/\/(.+?)mw\.jpg/;
-var res = comp.match("http://i2.pixiv.net/img28/img/seeker00/mobile/37975653_128x128.jpg\",,,\"http://i2.pixiv.net/img28/img/seeker00/mobile/37975653_480mw.jpg\",,,\"2013-08-22 00:16:55\",\"東方 西行寺幽々子 風景 魂魄妖夢 東方Project100users入り\"\"http://i2.pixiv.net/img28/img/seeker00/mobile/37975653_480mw.jpg\",\"");
-console.log(res);
-
-var source = "http://i2.pixiv.net/img28/img/seeker00/mobile/37975653_128x128.jpg\",,,\"http://i2.pixiv.net/img28/img/seeker00/mobile/37975653_480mw.jpg\",,,\"2013-08-22 00:16:55\",\"東方 西行寺幽々子 風景 魂魄妖夢 東方Project100users入り\"\"http://i2.pixiv.n975653_480mw.jpg\",\"";
-var comp = /http:\/\/(.+?)mw\.jpg/g;
-var arr = source.match(comp);
-console.log(arr);
-
-for (var i = 0; i < str.length; i++){
-	if (str[i] == '"'){
-		str
-	}
-}
-
-source.replace(/\"/, "")
-*/
